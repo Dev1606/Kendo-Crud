ac002d50f1f38529bb8ab154ee21695ee1a4bd39
Rahul Rana <140385300+RahulNRana@users.noreply.github.com>
2024-02-27 23:24:03 +0530

    AdminApi merged in development branch (#9)
    * Added AdminRepo Method In IEmpInteface & Also Add RepoClass Method In EmpAPIRepo & Added AdminApiController Method In MVCApicontroller (AdminAPi Task Completed)
    
    * Accidently all repo code copy paste so remove those unnecessary code in Apirepo and interface
    
    * Added APiAjaxController and ApiAjaxIndex with methods
    
    ---------
    
    Co-authored-by: JeminMolesalam <jeminmolesalam23@gmail.com>
    Co-authored-by: vensi2731 <vensiitaliya@gmail.com>

--- WebApi/Controllers/MVCApiController.cs
+++ WebApi/Controllers/MVCApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
 using WebApi.Repositories.API_Repositories;
 
 
@@ -18,6 +19,46 @@ namespace WebApi.Controllers
         }
 
         #region Admin API Calls
+
+        [HttpGet]
+        [Route("GetEmpData")]
+        public IActionResult GetEmpData()
+        {
+            var emplist = _empAPIInterface.GetEmpData();
+            return Ok(emplist);
+        }  
+
+        [HttpGet]
+        [Route("GetDropDepartment")]
+        public string[] GetDepartment()
+        {
+            return _empAPIInterface.GetDepartment();
+        }
+
+        [HttpGet]
+        [Route("GetEmpDetail")]
+        public IActionResult GetEmpDetail(int id)
+        {
+            var emp = _empAPIInterface.GetEmpDetail(id);
+            return Ok(emp);
+        }
+
+        [HttpPut]
+        [Route("UpdateEmpData")]
+        public IActionResult UpdateEmp(EmpModel emp)
+        {
+            _empAPIInterface.UpdateEmp(emp);
+            return Ok("Employee Updated Succcessfully");
+        }
+
+        [HttpDelete]
+        [Route("DeleteEmpData")]
+        public IActionResult DeleteEmp(int id)
+        {
+            _empAPIInterface.DeleteEmp(id);
+            return Ok("Employee Deleted Succcessfully");
+        }
+
         #endregion
 
         #region User API Calls
--- WebApi/Repositories/API_Repositories/IEmpAPIInterface.cs
+++ WebApi/Repositories/API_Repositories/IEmpAPIInterface.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Models;
 
 namespace WebApi.Repositories.API_Repositories
 {
     public interface IEmpAPIInterface
     {
-        
+        #region Admin Repo Methods
+        List<EmpModel> GetEmpData();
+        string[] GetDepartment();
+        EmpModel GetEmpDetail(int id);
+        bool DeleteEmp(int id);
+        bool UpdateEmp(EmpModel emp);
+        #endregion
+
+        #region User Repo Methods
+        #endregion 
+
     }
 }
