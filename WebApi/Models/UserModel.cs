1faed6b9c5dcb435cfbaaa01b77d06962e75bc3c
kodwaniishika <ishikakodwani717@gmail.com>
2024-02-27 09:08:51 +0530

    updated validations

--- WebApi/Models/UserModel.cs
+++ WebApi/Models/UserModel.cs
@@ -9,20 +9,25 @@ namespace WebApi.Models
     public class UserModel
     {
         public int c_uid { get; set; } = 0;
-        [Required(ErrorMessage = "Please  enter your username")]
-        public string c_uname { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string? c_uname { get; set; } = string.Empty;
+        //email
         [Required(ErrorMessage = "Please provide an email")]
-        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Please enter a valid email address.")]
-        public string c_uemail { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Please provide a password")]
-//         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z]).{6,}$
-// ", ErrorMessage = "Password must be atleast 6 characteras long,atleast one lowercase,uppercase,digits and special character")]
+         [RegularExpression(@"^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$",ErrorMessage ="Please enter a valid email")]
+        public string? c_uemail { get; set; } = string.Empty;
+
+        //Password
+         [RegularExpression(@"(?!^[0-9]$)(?!^[a-zA-Z]$)^([a-zA-Z0-9]{6,15})$", ErrorMessage = "At least one digit, one alphabetic character, no special characters, and 6-15 characters in length.")]
+        // [DataType(DataType.Password)]
+         [Required(ErrorMessage = "Password can't be blank!")]
+       
+        public string? c_password { get; set; } = string.Empty;
+        //confirm password
+        [Compare("c_password",ErrorMessage ="'Password' and 'confirm password' must match")]
         [DataType(DataType.Password)]
-        public string c_password { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Please confirm your password")]
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string c_confirmpassword { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Confirm password is required")]
+        public string? c_confirmpassword { get; set; } = string.Empty;
 
         // 0 -> User and 1 -> Admin
         public int c_role { get; set; } = 0;
