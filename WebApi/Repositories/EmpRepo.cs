93912dea5fd439428154810c4c1361597e2a5280
“datteshjadav” <jadavdattesh1@gmail.com>
2024-02-27 10:56:03 +0530

    GetEmpData() and GetDepartment() methods added in EmpRepo.

--- WebApi/Repositories/EmpRepo.cs
+++ WebApi/Repositories/EmpRepo.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
+using WebApi.Models;
 
 namespace WebApi.Repositories
 {
@@ -22,6 +24,64 @@ namespace WebApi.Repositories
         */
 
         #region Admin Repo Methods
+
+        public List<EmpModel> GetEmpData(){
+            var EmployeeList = new List<EmpModel>();
+
+            using(NpgsqlConnection con = new NpgsqlConnection(_ConnectionString)){
+                try{
+                    var qry = "SELECT c_empid, c_empname, c_empgender, c_dob, c_shift, c_department, c_empimage FROM mvc_master_project.t_emp;";
+                    using(NpgsqlCommand cmd = new NpgsqlCommand(qry,con)){
+                        con.Open();
+                        var reader = cmd.ExecuteReader();
+
+                        while(reader.Read()){
+
+                            var employee = new EmpModel{
+                                c_empid = (int)reader["c_empid"],
+                                c_empname = (string)reader["c_empname"],
+                                c_empgender = (string)reader["c_empgender"],
+                                c_dob = (DateTime)reader["c_dob"],
+                                c_shift = (string[])reader["c_shift"],
+                                c_department = (string)reader["c_department"],
+                                c_empimage = (string)reader["c_empimage"]
+                            };
+                            EmployeeList.Add(employee);
+                        }
+
+                    }
+
+                }catch(Exception e){
+                    Console.WriteLine("######nGet Employee Data error: "+e);
+                }finally{
+                    con.Close();
+                }
+            }
+
+            return EmployeeList;
+        }
+
+        public string[] GetDepartment(){
+            var Department = new List<string>();
+            using(NpgsqlConnection con = new NpgsqlConnection(_ConnectionString)){
+                try{
+                    con.Open();
+                    using(NpgsqlCommand cmd = new NpgsqlCommand("SELECT c_departmentid, c_department mvc_master_project.t_department;",con)){
+                        var reader = cmd.ExecuteReader();
+                        while(reader.Read()){
+                            Department.Add((string) reader["c_department"]);
+                        }
+                    }
+                }catch(Exception e){
+                    Console.WriteLine("#### Get Department data helper error ##### "+e);
+
+                }finally{
+                    con.Close();
+                }
+            }
+            return Department.ToArray();
+        }
+        
         #endregion
 
         #region User Repo Methods
