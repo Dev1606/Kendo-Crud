074f4712092b1570a658c69413ac854ae630c0b1
JeminMolesalam <jeminmolesalam23@gmail.com>
2024-02-27 19:51:05 +0530

    Added AdminRepo Method In IEmpInteface & Also Add RepoClass Method In EmpAPIRepo & Added AdminApiController Method In MVCApicontroller (AdminAPi Task Completed)

--- WebApi/Repositories/API_Repositories/EmpAPIRepo.cs
+++ WebApi/Repositories/API_Repositories/EmpAPIRepo.cs
@@ -2,11 +2,344 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
+using WebApi.Models;
 
 namespace WebApi.Repositories.API_Repositories
 {
     public class EmpAPIRepo : IEmpAPIInterface
     {
+        private readonly string? _ConnectionString;
+        public EmpAPIRepo(IConfiguration configuration)
+        {
+            _ConnectionString = configuration.GetConnectionString("ConStr");
+        }
+
+        /*
+            User **** conn **** as connection object.
+            Everthing must be enclosed with in try catch block with proper exception
+            _ConnectionString variable has the connection string.
+        */
+
+        #region Admin Repo Methods
+
+        //Get Employee Data
+        public List<EmpModel> GetEmpData()
+        {
+            var EmployeeList = new List<EmpModel>();
+
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    var qry = "SELECT c_empid, c_empname, c_empgender, c_dob, c_shift, c_department, c_empimage FROM mvc_master_project.t_emp;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(qry, con))
+                    {
+                        con.Open();
+                        var reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+
+                            var employee = new EmpModel
+                            {
+                                c_empid = (int)reader["c_empid"],
+                                c_empname = (string)reader["c_empname"],
+                                c_empgender = (string)reader["c_empgender"],
+                                c_dob = (DateTime)reader["c_dob"],
+                                c_shift = (string[])reader["c_shift"],
+                                c_department = (string)reader["c_department"],
+                                c_empimage = (string)reader["c_empimage"]
+                            };
+                            EmployeeList.Add(employee);
+                        }
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("###### Get Employee Data error: " + e);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            return EmployeeList;
+        }
+
+        //Get Department From Dropdown
+        public string[] GetDepartment()
+        {
+            List<string> Department = new List<string>();
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT c_departmentid, c_department FROM mvc_master_project.t_department;", con))
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Department.Add((string)reader["c_department"]);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("#### Get Department data error" + e);
+
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return Department.ToArray();
+        }
+
+        //Get Specific Employee Details
+        public EmpModel GetEmpDetail(int id)
+        {
+            var emp = new EmpModel();
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    var qry = "SELECT c_empid, c_empname, c_empgender, c_dob, c_shift, c_department, c_empimage FROM mvc_master_project.t_emp WHERE c_empid = @id";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(qry, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        con.Open();
+                        var reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+
+                            emp = new EmpModel
+                            {
+                                c_empid = (int)reader["c_empid"],
+                                c_empname = (string)reader["c_empname"],
+                                c_empgender = (string)reader["c_empgender"],
+                                c_dob = (DateTime)reader["c_dob"],
+                                c_shift = (string[])reader["c_shift"],
+                                c_department = (string)reader["c_department"],
+                                c_empimage = (string)reader["c_empimage"]
+                            };
+                        }
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("###### Get Employee Detail error: " + e);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return emp;
+        }
+
+        //Delete Employee
+        public bool DeleteEmp(int id)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    var qry = "DELETE FROM mvc_master_project.t_emp WHERE c_empid=@id;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(qry, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("#### Delete Employee Data Error : " + e);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return true;
+        }
+
+        //Update Employee
+        public bool UpdateEmp(EmpModel emp)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    var qry = "UPDATE mvc_master_project.t_emp SET c_empname=@c_empname, c_empgender=@c_empgender, c_dob=@c_dob, c_shift=@c_shift, c_department=@c_department, c_empimage=@c_empimage WHERE c_empid=@c_empid;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(qry, con))
+                    {
+                        cmd.Parameters.AddWithValue("@c_empid", emp.c_empid);
+                        cmd.Parameters.AddWithValue("@c_empname", emp.c_empname);
+                        cmd.Parameters.AddWithValue("@c_empgender", emp.c_empgender);
+                        cmd.Parameters.AddWithValue("@c_dob", emp.c_dob);
+                        cmd.Parameters.AddWithValue("@c_shift", emp.c_shift);
+                        cmd.Parameters.AddWithValue("@c_department", emp.c_department);
+                        cmd.Parameters.AddWithValue("@c_empimage", emp.c_empimage);
+                        Console.WriteLine("Emp details @ repo"+ emp.c_empid+emp.c_empname+ emp.c_empgender+emp.c_dob+emp.c_shift+ emp.c_department+emp.c_empimage);
+
+                        // string c_shift = string.Join(",", emp.c_shift);
+                        // cmd.Parameters.AddWithValue("@c_shift", emp.c_shift);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("####  Update Employee Data Error : " + e);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region User Repo Methods
+
+        //Get department In Dropdwn
+        // public string[] GetDepartment()
+        // {
+        //     var department = new List<string>();
+        //     using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+        //     {
+        //         try
+        //         {
+        //             con.Open();
+        //             using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT c_department FROM mvc_master_project.t_department;", con))
+        //             {
+        //                 var reader = cmd.ExecuteReader();
+        //                 while (reader.Read())
+        //                 {
+        //                     department.Add((string)reader["c_department"]);
+        //                 }
+        //             }
+        //         }
+        //         catch (Exception e)
+        //         {
+        //             Console.WriteLine("#### Get Department helper error ##### " + e);
+        //         }
+        //         finally
+        //         {
+        //             con.Close();
+        //         }
+        //     }
+        //     return department.ToArray();
+        // }
         
+        // UserGetEmpData
+
+
+        //Get UserEmpData
+        public List<EmpModel> UserGetEmpData()
+        {
+            var Employees = new List<EmpModel>();
+
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    var qry = "SELECT c_empid, c_empname, c_empgender, c_dob, c_shift, c_department, c_empimage FROM mvc_master_project.t_emp;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(qry, con))
+                    {
+                        con.Open();
+                        var reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+
+                            var Employee = new EmpModel
+                            {
+                                c_empid = (int)reader["c_empid"],
+                                c_empname = (string)reader["c_empname"],
+                                c_empgender = (string)reader["c_empgender"],
+                                c_dob = (DateTime)reader["c_dob"],
+                                c_shift = (string[])reader["c_shift"],
+                                c_department = (string)reader["c_department"],
+                                c_empimage = (string)reader["c_empimage"]
+                            };
+                            Employees.Add(Employee);
+                        }
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("------> Get Employee Helper error: " + e);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return Employees;
+        }
+
+        //UserAddEmp Data
+        public bool UserAddEmpData(EmpModel employee)
+        {
+            int rowseffected = 0;
+            //Adding Employee
+            using (NpgsqlConnection con = new NpgsqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    var qry = "INSERT INTO mvc_master_project.t_emp(c_empname, c_empgender, c_dob, c_shift, c_department, c_empimage) VALUES (@c_empname, @c_empgender, @c_dob, @c_shift, @c_department, @c_empimage);";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(qry, con))
+                    {
+                        cmd.Parameters.AddWithValue("@c_empname", employee.c_empname);
+                        cmd.Parameters.AddWithValue("@c_empgender", employee.c_empgender);
+                        cmd.Parameters.AddWithValue("@c_dob", employee.c_dob);
+                        string shift = string.Join(",", employee.c_shift);
+                        cmd.Parameters.AddWithValue("@c_shift", employee.c_shift);
+                        cmd.Parameters.AddWithValue("@c_department", employee.c_department);
+                        cmd.Parameters.AddWithValue("@c_empimage", employee.c_empimage);
+                        
+
+                        con.Open();
+                        rowseffected = cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("-------> Add Employee Helper : " + e);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            if (rowseffected > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion 
+
     }
 }
+
--- WebApi/Repositories/API_Repositories/IEmpAPIInterface.cs
+++ WebApi/Repositories/API_Repositories/IEmpAPIInterface.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Models;
 
 namespace WebApi.Repositories.API_Repositories
 {
     public interface IEmpAPIInterface
     {
-        
+        #region Admin Repo Methods
+        List<EmpModel> GetEmpData();
+        string[] GetDepartment();
+        EmpModel GetEmpDetail(int id);
+        bool DeleteEmp(int id);
+        bool UpdateEmp(EmpModel emp);
+        #endregion
+
+        #region User Repo Methods
+
+        // String[] GetDepartment();
+        List<EmpModel> UserGetEmpData();
+        bool UserAddEmpData(EmpModel employee);
+
+        #endregion 
+
     }
 }
