bfe7111ab1fd80e24e14c7d6cbd4408d9eb74a9a
vensi2731 <vensiitaliya@gmail.com>
2024-03-03 11:30:24 +0530

    added in AdminKendoComponent pendding editing...

--- MVC/Controllers/KendoComponentController.cs
+++ MVC/Controllers/KendoComponentController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApi.Models;
+using WebApi.Repositories;
 
 namespace MVC.Controllers
 {
@@ -12,13 +14,19 @@ namespace MVC.Controllers
     public class KendoComponentController : Controller
     {
         private readonly ILogger<KendoComponentController> _logger;
+         private readonly IEmpInterface _empRepo;
+          private readonly IWebHostEnvironment _hostingEnvironment;
+          private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public KendoComponentController(ILogger<KendoComponentController> logger)
+        public KendoComponentController(ILogger<KendoComponentController> logger,IEmpInterface empRepo, IWebHostEnvironment environment, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _empRepo = empRepo;
+            _hostingEnvironment = environment;
+            _httpContextAccessor = httpContextAccessor;
         }
 
-        public IActionResult Index()
+        public IActionResult AdminIndex()
         {
             return View();
         }
@@ -30,11 +38,104 @@ namespace MVC.Controllers
         {
             return View();
         }
+        #region Admin
+        
+        [Produces("application/json")]
+        [HttpGet]
+        public IActionResult AdminGetEmpData()
+        {
+            var empData = _empRepo.GetEmpData();
+            return Json(empData);
+        }
+
+        [HttpGet]
+        public string[] GetDepartment()
+        {
+            return _empRepo.GetDepartment();
+        }
+
+        [HttpGet]
+        public IActionResult GetEmpDetail(int id)
+        {
+            var empDetail = _empRepo.GetEmpDetail(id);
+            return Json(empDetail);
+        }
+
+        [HttpGet]
+        public IActionResult AdminUpdateEmpData(int id)
+        {
+            ViewBag.Departments = _empRepo.GetDepartment();
+            var empUpdate = _empRepo.GetEmpDetail(id);
+            return Json(empUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult AdminUpdateEmpData(EmpModel emp)
+        {
+
+            Console.WriteLine(emp);
+            Console.WriteLine("DOB"+emp.c_dob);
+            //Code For File Upload:
+            if (emp.Image != null && emp.Image.Length > 0)
+            {
+                var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploadsimg");
+                var uniqueFileName = Guid.NewGuid().ToString() + "_" + emp.Image.FileName;
+                //var uniqueFileName =  item.Image.FileName; //To Get Only File Name
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    emp.Image.CopyTo(stream);
+                }
+
+                // Save The File Path To Our DB Table In c_image Field:
+                emp.c_empimage = uniqueFileName;
+            }
+            
+            _empRepo.UpdateEmp(emp);
+            return Json(new{success=true,message="Employee updated"});
+        }
+
+        [HttpGet]
+        public IActionResult AdminDeleteEmp(int id)
+        {
+            var empDelete = _empRepo.GetEmpDetail(id);
+            return View(empDelete);
+        }
+
+        [HttpPost]
+        public IActionResult AdminDeleteEmpConfirm(int id)
+        {
+            Console.WriteLine("Delete called"+id);
+            _empRepo.DeleteEmp(id);
+            return Json(new{success=true,message="Employee updated"});
+        }
+        //  static string file="";
+        [HttpPost]
+       public IActionResult UploadPhoto(EmpModel emp)
+        {
+            if (emp.Image!= null)
+            {
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + emp.Image.FileName;
+                string filepath = Path.Combine(_hostingEnvironment.WebRootPath, "uploadsimg",uniqueFileName);
+
+                using (var stream = new FileStream(filepath, FileMode.Create))
+                {
+
+                    emp.Image.CopyTo(stream);
+                }
+
+                // file = uniqueFileName;
+            }
+
+            return Json("Image Uploaded");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
         }
+        #endregion
     }
 }
---
+++ MVC/wwwroot/js/AdminKendoComponentMVC.js
@@ -0,0 +1,173 @@
+$(document).ready(function () {
+    var ImageFileName = "";
+    console.log("Welcome Employee");
+    GetAll();
+    getDropdownValues();
+    hideAlerts();
+    initializeGenderRadioButtons();
+    initializeShiftCheckboxes();
+    initializeDatePicker();
+    function formatDateForInput(dateString) {
+        const dateObj = new Date(dateString);
+        const formattedDate = dateObj.toISOString().slice(0, 10);
+        return formattedDate;
+    }
+    function hideAlerts() {
+        $('#messageSuccess').hide();
+        $('#messageFail').hide();
+
+    }
+    function successMsg(str) {
+        $("#messageSuccess").text('');
+        $("#messageSuccess").append(str);
+        $("#messageSuccess").show().delay(3000).fadeOut();
+    }
+    function alertMsg(str) {
+        $("#messageFail").text('');;
+        $("#messageFail").append(str);
+        $("#messageFail").show().delay(3000).fadeOut();
+    }
+    // for getting dropdown
+    function getDropdownValues() {
+        var dropdown = $("#EmpDepartment, #EditEmpDepartment");
+        dropdown.empty();
+        $.ajax({
+            url: '/KendoComponent/GetDepartment',
+            type: 'GET',
+            success: function (data) {
+                dropdown.kendoDropDownList({
+                    dataSource: data,
+                    optionLabel: "Select Department" 
+                });
+            }
+        });
+    }
+
+    //get
+    function GetAll() {
+        var table = $('#TableContent');
+        table.empty();
+        $.ajax({
+            type: "GET",
+            url: "/KendoComponent/AdminGetEmpData",
+            dataType: 'json',
+            success: function (emp) {
+                emp.forEach(function (emp) {
+                    var row = '<tr>';
+                    row += '<td>' + emp.c_empid + '</td>';
+                    row += '<td>' + emp.c_empname + '</td>';
+                    row += '<td>' + emp.c_empgender + '</td>';
+                    row += '<td>' + emp.c_dob + '</td>';
+                    row += '<td>' + emp.c_shift + '</td>';
+                    row += '<td>' + emp.c_department + '</td>';
+                    row += '<td><img src="../wwwroot/uploadsimg/' + emp.c_empimage + '"></td>';
+                    row += '<td>';
+                    row += '<div class="d-flex justify-content-between">';
+                    row += '<button type="button" id="edit" class="btn btn-outline-success edit" data-id="' + emp.c_empid + '" data-bs-toggle="modal" data-bs-target="#EditEmpModal">Edit</button>';
+                    row += '<button type="button" id="del" class="btn btn-outline-danger delete" data-id="' + emp.c_empid + '">Delete</button>';
+
+                    row += '</div>';
+                    row += '</td>';
+                    row += '</tr>';
+                    table.append(row);
+                });
+            }
+        });
+    }
+
+    function initializeGenderRadioButtons() {
+        $("#EditEmpGender").kendoRadioGroup({
+            layout: "horizontal",
+            items: [
+                { label: "Male", value: "Male" },
+                { label: "Female", value: "Female" },
+                { label: "Other", value: "Other" }
+            ]
+        });
+    }
+
+    function initializeShiftCheckboxes() {
+        $("#EditEmpShift").kendoCheckBoxGroup({
+            items: [
+                { label: "Morning", value: "Morning" },
+                { label: "Afternoon", value: "Afternoon" },
+                { label: "Night", value: "Night" }
+            ],
+            layout: "horizontal"
+        });
+    }
+
+    function initializeDatePicker() {
+        $("#EditEmpDob").kendoDatePicker({
+            format: "yyyy-MM-dd"
+        });
+    }
+    //edit
+    $(document).on('click', '#edit', function () {
+        var eid = $(this).data('id');
+        console.log(eid);
+        $.get("/KendoComponent/GetEmpDetail", { id: eid }, function (employee) {
+            var ImagePath = "~/uploadsimg/" + employee.c_empimage;
+            ImageFileName = employee.c_empimage;
+            console.log(employee);
+            $('#Empid').attr('data-id', eid);
+            $('#EditEmpName').val(employee.c_empname);
+            $('#EditEmpGender').data("kendoRadioGroup").value(employee.c_empgender);
+            $('#EditEmpDob').val(formatDateForInput(employee.c_dob));
+            $("#EditEmpDepartment").data("kendoDropDownList").value(employee.c_department);
+            $("#EditEmpShift").data("kendoCheckBoxGroup").value(employee.c_shift);
+            $('#EditImage').attr('src', ImagePath);
+            $('#EditImage').attr('data-value', employee.c_empimage);
+            $('#EditModel').modal('show');
+        });
+    });
+
+    $("#FinalEditBtn").click(function () {
+        var formData = {
+            c_empid: $("#EditEmpId").val(),
+            c_empname: $("#EditName").val(),
+            c_empgender: $('#EditEmpGender').data("kendoRadioGroup").value(),
+            c_dob: $("#EditEmpDob").val(),
+            c_shift: $("#EditEmpShift").data("kendoCheckBoxGroup").value(),
+            c_department: $("#EditEmpDepartment").data("kendoDropDownList").value(),
+            c_empimage: ImageFileName,
+        };
+
+        $.ajax({
+            url: "/KendoComponent/AdminUpdateEmpData",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function (response) {
+                // Handle success
+                $("#messageSuccess").text(response.message).show();
+                $("#messageFail").hide();
+            },
+            error: function (xhr, status, error) {
+                // Handle error
+                $("#messageFail").text(xhr.responseText).show();
+                $("#messageSuccess").hide();
+            }
+        });
+    });
+
+    //Delete employee
+    $(document).on('click', '#del', function () {
+        var Id = $(this).data('id');
+        console.log(Id);
+        if (confirm("Are you sure you want to delete this data?")) {
+            $.ajax({
+                url: '/KrndoComponent/AdminDeleteEmpConfirm',
+                type: 'POST',
+                data: {id : Id},
+                dataType: 'json',
+                success: function (data) {
+                    GetAll();
+                    alert(data.message);
+                }
+            });
+        }
+    });
+
+
+});
