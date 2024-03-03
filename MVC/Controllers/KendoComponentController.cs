0b485c20eccc409bee98f3ebfc7165cdd7c1f893
JeminMolesalam <jeminmolesalam23@gmail.com>
2024-03-03 12:59:51 +0530

    Added User Side Metod In KendoComponentController & And Added UserKendoMVC View In KendoComponent View & Also Added In UserKendoComponent.js File In Js Folder In wwwrooot Folder.

--- MVC/Controllers/KendoComponentController.cs
+++ MVC/Controllers/KendoComponentController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApi.Models;
+using WebApi.Repositories;
+using WebApi.Repositories.API_Repositories;
 
 namespace MVC.Controllers
 {
@@ -12,10 +15,16 @@ namespace MVC.Controllers
     public class KendoComponentController : Controller
     {
         private readonly ILogger<KendoComponentController> _logger;
+        private readonly IEmpInterface _empRepo;
+        private readonly IWebHostEnvironment _environment;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public KendoComponentController(ILogger<KendoComponentController> logger)
+        public KendoComponentController(ILogger<KendoComponentController> logger,IEmpInterface empRepo, IWebHostEnvironment environment, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _empRepo = empRepo;
+            _environment = environment;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
@@ -30,6 +39,55 @@ namespace MVC.Controllers
         {
             return View();
         }
+
+        #region UserSide
+
+        public IActionResult UserKendoMVC()
+        {
+            return View();
+        }
+
+        [Produces("application/json")]
+        [HttpGet]
+        public IActionResult UserGetEmpData()
+        {
+            var empData = _empRepo.UserGetEmpData();
+            return Json(empData);
+        }
+
+        public IActionResult UserAddEmpData(EmpModel emp)
+        {
+            emp.c_empimage = file;
+            var empData = _empRepo.UserAddEmpData(emp);
+            return Json(empData);
+        }
+        [HttpGet]
+        public string[] GetDepartment()
+        {
+            return _empRepo.GetDepartment();
+        }
+        static string file = "";
+
+        public IActionResult UploadPhoto(EmpModel emp)
+        {
+            if (emp.Image!= null)
+            {
+                string filename = emp.Image.FileName;
+                string filepath = Path.Combine(_environment.WebRootPath, "uploadsimg", filename);
+
+                using (var stream = new FileStream(filepath, FileMode.Create))
+                {
+
+                    emp.Image.CopyTo(stream);
+                }
+
+              file = filename;
+
+            }
+
+            return Json("Image Uploaded");
+        }
+        #endregion
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
