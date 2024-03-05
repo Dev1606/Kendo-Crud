b662824f63d7cb89453bb93e2e7e0caa384b6a44
vensi2731 <vensiitaliya@gmail.com>
2024-03-05 10:24:05 +0530

    changes in adminKendoCompoinent src

--- MVC/wwwroot/js/AdminKendoComponentMVC.js
+++ MVC/wwwroot/js/AdminKendoComponentMVC.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
                     row += '<td>' + emp.c_dob + '</td>';
                     row += '<td>' + emp.c_shift + '</td>';
                     row += '<td>' + emp.c_department + '</td>';
-                    row += '<td><img src="../wwwroot/uploadsimg/' + emp.c_empimage + '"></td>';
+                    row += '<td><img src="/uploadsimg\\'+emp.c_empimage +'" alt="Image Not Found" style="height: 15%;width:15%;"></td>';
                     row += '<td>';
                     row += '<div class="d-flex justify-content-between">';
                     row += '<button type="button" id="edit" class="btn btn-outline-success edit" data-id="' + emp.c_empid + '" data-bs-toggle="modal" data-bs-target="#EditEmpModal">Edit</button>';
