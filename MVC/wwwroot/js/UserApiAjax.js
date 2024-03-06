d34428c0c1b32ba2d2486ba3d3742eb344444b99
Dev1606 <141394467+Dev1606@users.noreply.github.com>
2024-03-06 09:41:05 +0530

    User api Branch Merge in Development Branch (#20)
    * Priview Image at User Site
    
    * ApiImg insert done with mvc view in user side
    
    ---------
    
    Co-authored-by: RahulNRana <rjariwala245@gmail.com>

--- MVC/wwwroot/js/UserApiAjax.js
+++ MVC/wwwroot/js/UserApiAjax.js
@@ -15,8 +15,7 @@ $(document).ready(function () {
         GetToken();
     }
 
-    $('#LogoutBtn').on('click', function ()
-    {
+    $('#LogoutBtn').on('click', function () {
         localStorage.removeItem('token');
         window.location = '/UserApi/Login';
     });
@@ -52,7 +51,7 @@ $(document).ready(function () {
     }
     // for getting dropdown
     function getDropdownValues() {
-        var dropdown = $("#EditEmpDepartment");
+        var dropdown = $("#c_department");
         dropdown.empty();
         $.ajax({
             url: 'https://localhost:7068/api/MVCApi/GetDropDepartment',
@@ -70,39 +69,57 @@ $(document).ready(function () {
         });
     }
 
-    //Add User Data:
-    $('#printbtn').on('click', function () {
-        AddUser();
-        console.log("clicked");
-        GetAllUser();
-    });
-    function AddUser() {
-        var formData = new FormData();
-        formData.append('c_empname', $('#EmpName').val());
-        formData.append('c_empgender', $('input[name="rdbtn"]:checked').val());
-        formData.append('c_dob', $('#EmpDob').val().split('T')[0]);
-        $('input[name="chkbox"]:checked').each(function () {
-            formData.append('c_shift[]', $(this).val());
-        });
-        formData.append('c_department', $('#EditEmpDepartment').val());
-        //formData.append('Image',$('#EmpImage')[0].files[0]);
+    // //Add User Data:
+    // $('#printbtn').on('click', function () {
+    //     AddUser();
+    //     console.log("clicked");
+    //     GetAllUser();
+    // });
+    // function AddUser() {
+    //     var formData = new FormData();
+    //     formData.append('c_empname', $('#EmpName').val());
+    //     formData.append('c_empgender', $('input[name="rdbtn"]:checked').val());
+    //     formData.append('c_dob', $('#EmpDob').val().split('T')[0]);
+    //     $('input[name="chkbox"]:checked').each(function () {
+    //         formData.append('c_shift[]', $(this).val());
+    //     });
+    //     formData.append('c_department', $('#EditEmpDepartment').val());
+    //     //formData.append('Image',$('#EmpImage')[0].files[0]);
 
-        //save kri ne run karavje
+    //     //save kri ne run karavje
 
-        var img = $('#EmpImage')[0].files[0];
+    //     var img = $('#EmpImage')[0].files[0];
+    //     $.ajax({
+    //         url: 'https://localhost:7068/api/MVCApi/UserAddEmpData',
+    //         type: 'POST',
+    //         dataType: 'json',
+    //         contentType: 'application/json',
+    //         data: { emp: JSON.stringify(formData), file: img },
+    //         success: function (data) {
+    //             GetAllUser();
+    //             successMsg(data.message);
+    //         }
+    //     });
+    // }
+    $("#addEmpForm").submit(function (event) {
+        event.preventDefault();
+        var formData = new FormData(this);
+        console.log(formData);
         $.ajax({
-            url: 'https://localhost:7068/api/MVCApi/UserAddEmpData',
-            type: 'POST',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: { emp: JSON.stringify(formData), file: img },
-            success: function (data) {
+            url: "https://localhost:7068/api/MVCApi/UserAddEmpData",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
                 GetAllUser();
-                successMsg(data.message);
+                console.log(response);
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
             }
         });
-    }
-
+    });
     //Get All User Details:
     function GetAllUser() {
         var table = $('#TableContent11');
@@ -122,12 +139,8 @@ $(document).ready(function () {
                     row += '<td>' + emp.c_dob + '</td>';
                     row += '<td>' + emp.c_shift + '</td>';
                     row += '<td>' + emp.c_department + '</td>';
-                    row += '<td>' + emp.c_empimage + '</td>';
+                    row += '<td> <img src="' + emp.c_empimage + '" alt="Image Not Found" style="height:50%;width:50%;"></td>';
                     row += '<td>';
-                    // row += '<div class="d-flex justify-content-between">';
-                    // row += '<button type="button" id="edit" class="btn btn-outline-success edit" data-id="' + emp.c_empid + '">Edit</button>';
-                    // row += '<button type="button" id="del" class="btn btn-outline-danger delete" data-id="' + emp.c_empid + '">Delete</button>';
-
                     row += '</div>';
                     row += '</td>';
                     row += '</tr>';
@@ -145,7 +158,7 @@ $(document).ready(function () {
         $('#date').val("");
         $('#salary').val("");
     }
-    
+
     function fetchUserData(token) {
         var username = "";
         $.ajax({
@@ -158,8 +171,8 @@ $(document).ready(function () {
                 $.ajax({
                     url: "/User/SetUserData",
                     method: "POST",
-                    data: { username: username }, 
-                    async:false,
+                    data: { username: username },
+                    async: false,
                     success: function (response) {
                         if (response.success) {
                             // Handle successful response, e.g., display a success message
